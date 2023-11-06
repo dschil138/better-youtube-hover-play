a2dc0165c3715286c80066848211a1809e16b823
dschil138 <davidschiller138@gmail.com>
2023-11-05 19:59:58 -0600

    Totally now, much simpler approach, using the native functionality and  a scroll flag. Works perfectly now. No longer requires long click, videos only autoplay if you moved your mouse to hover over them, not if you just scrolled over them. If you do  want to autoplay the video you have just scrolled to, just move your mouse slightly and it will autoplay

---
+++ README.md
@@ -0,0 +1,3 @@
+# No Autoplay on Scroll Youtube
+
+The YouTube homepage autoplays video previews anytime you hover your mouse over a thumbnail. This can be very distracting. This Chrome extension turns that off while scrolling, so video previews will only autoplay if you actually purposefully moved your mouse to hover over that video.
--- content.js
+++ content.js
@@ -1,157 +1,79 @@
-let longPress = false;
+let isScrolling = false;
+let enterListeners = 0;
+let lastKnownMousePosition = { x: 0, y: 0 };
 
+window.addEventListener('mousemove', (e) => {
+  // If the mouse moved and scrolling was in progress, we reset the scrolling flag
+  // and manually trigger mouseenter on the element below the cursor
+  if (isScrolling) {
+    isScrolling = false;
+    console.log('mouse moved, checking for element below');
 
-// function logElementDetails(element) {
-//   console.log('Element details for:', element);
+    let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+    if (elemBelow) {
+      // Dispatch a new mouseenter event
+      elemBelow.dispatchEvent(new MouseEvent('mouseenter', {
+        bubbles: true,
+        cancelable: true,
+        view: window
+      }));
+      console.log('Dispatched mouseenter to', elemBelow);
+    }
+  }
+  lastKnownMousePosition = { x: e.clientX, y: e.clientY };
+});
 
-//   // Log all attributes
-//   console.log('Attributes:');
-//   for (let attr of element.attributes) {
-//     console.log(` ${attr.name}="${attr.value}"`);
-//   }
-
-//   // Log all classes
-//   console.log('Classes:');
-//   console.log(' ' + element.className); // className returns all classes as a single string
-// }
-
-
-
-
-function freezeAttributes(element) {
-  console.log('Freezing attributes for:', element);
-
-  // Create a mutation observer to watch for changes to the attributes
-  const attributeObserver = new MutationObserver((mutations) => {
-    mutations.forEach((mutation) => {
-      // If a long press is detected, do not remove the attributes
-      if (longPress) {
-        return;
-      }
-
-      // Check if the 'active' attribute was added or modified
-      if (mutation.type === 'attributes' && mutation.attributeName === 'active') {
-        console.log('Active attribute changed for:', mutation.target);
-        // Remove the 'active' attribute to prevent playback
-        mutation.target.removeAttribute('active');
-        console.log('Active attribute removed:', mutation.target);
-      }
-
-      // Check if the 'playing' attribute was added or modified
-      if (mutation.type === 'attributes' && mutation.attributeName === 'playing') {
-        console.log('Playing attribute changed for:', mutation.target);
-        // Remove the 'playing' attribute to prevent playback
-        mutation.target.removeAttribute('playing');
-        console.log('Playing attribute removed:', mutation.target);
-      }
-
-    });
-  });
-
-  // Start observing the element for attribute changes
-  attributeObserver.observe(element, { attributes: true });
-  console.log('Attribute observer set for:', element);
-}
-
-
-freezeAttributes(document.querySelector('ytd-video-preview'));
+window.addEventListener('wheel', () => {
+  isScrolling = true;
+}, { passive: true });
 
 
 
 
-// Function to handle the long press logic
-function handleLongPress(thumbnail, previewElement, linkElement) {
-  let longPressTimer;
-  const longPressDuration = 250; // Duration in milliseconds to detect long press
-
-  // Attach event listeners to the thumbnail
-  thumbnail.addEventListener('mousedown', function (e) {
-    // Prevent the default hover behavior
+function handleMouseEnter(e) {
+  if (isScrolling) {
     e.preventDefault();
     e.stopImmediatePropagation();
     e.stopPropagation();
-
-    // Start a timer to detect long press
-    longPressTimer = setTimeout(function () {
-      longPress = true;
-      // Set the href attribute of the link element
-      const videoUrl = thumbnail.getAttribute('href');
-      linkElement.setAttribute('href', videoUrl);
-
-      // Add attributes to the preview element to start playback
-      previewElement.setAttribute('active', '');
-      previewElement.setAttribute('playing', '');
-      console.log('Playback started due to long press.');
-    }, longPressDuration);
-  });
-
-  thumbnail.addEventListener('mouseup', function (e) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-    e.stopPropagation();
-    clearTimeout(longPressTimer);
-    longPress = false;
-
-    // Remove attributes to stop playback if it started
-    if (previewElement.hasAttribute('active') || previewElement.hasAttribute('playing')) {
-      previewElement.removeAttribute('active');
-      previewElement.removeAttribute('playing');
-      previewElement.setAttribute('hide-volume-controls', '');
-      previewElement.setAttribute('hidden', '');
-      console.log('Playback stopped due to mouse release.');
-    }
-  });
-
-  thumbnail.addEventListener('mouseleave', function (e) {
-    clearTimeout(longPressTimer);
-    longPress = false;
-  });
-}
-
-function initLongPressForThumbnails() {
-  const previewElement = document.querySelector('ytd-video-preview');
-  const linkElement = document.querySelector('a.ytp-title-link.yt-uix-sessionlink');
-
-  // Find all the thumbnail elements and initialize long press handling
-  const thumbnails = document.querySelectorAll('a.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail');
-  thumbnails.forEach(thumbnail => {
-    handleLongPress(thumbnail, previewElement, linkElement);
-  });
-}
-
-setTimeout(initLongPressForThumbnails, 1500);
-
-// This function is called when new nodes are added to the DOM
-function onDomChange(mutations, observer) {
-  for (let mutation of mutations) {
-    if (mutation.type === 'childList' && mutation.addedNodes.length) {
-      mutation.addedNodes.forEach(node => {
-        // Check if the added node is a thumbnail or contains thumbnails
-        if (node.matches && node.matches('a.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail')) {
-          handleLongPress(node, document.querySelector('ytd-video-preview'), document.querySelector('a.ytp-title-link.yt-uix-sessionlink'));
-        } else if (node.querySelectorAll) {
-          const thumbnails = node.querySelectorAll('a.yt-simple-endpoint.inline-block.style-scope.ytd-thumbnail');
-          thumbnails.forEach(thumbnail => {
-            handleLongPress(thumbnail, document.querySelector('ytd-video-preview'), document.querySelector('a.ytp-title-link.yt-uix-sessionlink'));
-          });
-        }
-      });
-    }
   }
 }
 
-// Initialize the MutationObserver
-function initMutationObserver() {
-  const observer = new MutationObserver(onDomChange);
-  
-  // Observe the body or another large container that will contain the thumbnails
+
+function addMouseEnterListeners(elements) {
+  elements.forEach(element => {
+    element.addEventListener('mouseenter', handleMouseEnter, true);
+    enterListeners++;
+  });
+}
+
+function observeDOMChanges() {
+  const selector = '#dismissible.style-scope';
+
+  const initialElements = document.querySelectorAll(selector);
+  addMouseEnterListeners(initialElements);
+
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+      if (mutation.addedNodes && mutation.addedNodes.length > 0) {
+        mutation.addedNodes.forEach((node) => {
+          if (node.nodeType === 1 && node.matches(selector)) {
+            addMouseEnterListeners([node]);
+          }
+          if (node.nodeType === 1 && node.querySelectorAll) {
+            const children = node.querySelectorAll(selector);
+            addMouseEnterListeners(children);
+          }
+        });
+      }
+    });
+  });
+
   observer.observe(document.body, {
     childList: true,
     subtree: true
   });
 
-  console.log('MutationObserver set up to watch for DOM changes.');
+  console.log('MutationObserver set up, added', enterListeners, 'mouseenter listeners');
 }
 
-// Call this function when the page has initially loaded
-initMutationObserver();
+observeDOMChanges();
